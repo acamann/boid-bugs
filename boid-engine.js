d86d5741dc4b2b3b9b9b5ccbbc26c90795922229
Andy Camann <acamann@gmail.com>
2020-02-22 13:10:13 -0600

    appropriate size and speed

--- boid-engine.js
+++ boid-engine.js
@@ -1,14 +1,4 @@
 
-// NOTES
-// to "normalize" a vector: divide x and y by magnitude
-// subtract two positions to calculate "displacement"
-//   "magnitude" is the distance between the two = sqrt(x^2 + y^2)
-//   "normalized" "displacement" is the direction to move to get closer to it
-
-// start with 2d, then consider how to do 3d??
-// start in javascript to understand logic, then move to C#
-
-
 // VECTOR OBJECT
 
 class Vector {
@@ -22,7 +12,7 @@ class Vector {
     }
 
     getMagnitude() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+       return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
     normalize() {
@@ -46,16 +36,26 @@ class Vector {
         this.y /= divisor;
     }
 
+    multiplyBy(factor) {
+        this.x *= factor;
+        this.y *= factor;
+    }
+
     static subtract(v1, v2) {
         return new Vector (v1.x - v2.x, v1.y - v2.y);
     }
     static divide(v1, divisor) {
         return new Vector (v1.x / divisor, v1.y / divisor);
     }
+    static distance(v1, v2) {
+        return Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2));
+    }
 }
 
 
+
 // BOIDBUG OBJECT
+
 class BoidBug {
 
     constructor( x , y , v1 = 0, v2 = 0 ) {
@@ -66,6 +66,7 @@ class BoidBug {
 }
 
 
+
 // BOID-ENGINE
 
 let boidBugs = [];
@@ -73,10 +74,17 @@ let boidField;
 let fieldWidth;
 let fieldHeight;
 
-const speedFactor = 0.001;
-const COHESION_FACTOR = 200;
-const SEPARATION_FACTOR = 8;
-const ALIGNMENT_FACTOR = 8;
+const AVOID_NEIGHBOR_DISTANCE = 25;
+const AVOIDANCE_FACTOR = 0.001;
+
+const ALIGN_NEIGHBOR_DISTANCE = 200;
+const ALIGNMENT_FACTOR = 0.1;
+
+const APPROACH_NEIGHBOR_DISTANCE = 200;
+const COHESION_FACTOR = 0.0001;
+
+const AVOID_WALL_FACTOR = 1.5;
+
     
 function infest() {
     boidField = document.getElementById("boid-field");
@@ -104,7 +112,7 @@ function createBoidBugObjects() {
     fieldWidth = boidField.clientWidth;
     fieldHeight = boidField.clientHeight;
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 100; i++) {
         boidBugs.push(new BoidBug(Math.random() * fieldWidth, Math.random() * fieldHeight, (2 * Math.random()) - 1, (2 * Math.random()) -1 ));
     }
 }
@@ -114,23 +122,28 @@ function addBugElementsToField() {
         let boidElement = document.createElementNS("http://www.w3.org/2000/svg", "image");
         boidElement.setAttribute("id", `bug-${index}`);
         boidElement.setAttribute("href", "img/bug-solid.svg");
-        boidElement.setAttribute("width", "8px");
-        boidElement.setAttribute("height", "8px");
+        boidElement.setAttribute("width", "16px");
+        boidElement.setAttribute("height", "16px");
         boidField.append(boidElement);
     });
 }
 
 function moveBoidBugs() {
     boidBugs.forEach(boidBug => {
-        alignmentVector = alignment(boidBug);
-        separationVector = separation(boidBug);
-        cohesionVector = cohesion(boidBug);
-                
-        boidBug.vector.add(alignmentVector); 
-        boidBug.vector.add(cohesionVector); 
-        boidBug.vector.add(separationVector);
+        let avoidVector = avoid(boidBug);
+        let alignVector = align(boidBug);
+        let approachVector = approach(boidBug);
+        let avoidWallsVector = avoidWalls(boidBug);
+        
+        boidBug.vector.add(avoidVector)
+        boidBug.vector.add(alignVector)
+        boidBug.vector.add(approachVector)
+        boidBug.vector.add(avoidWallsVector);
+        
+        boidBug.vector.normalize();     
 
         boidBug.position.add(boidBug.vector);
+
     })
 }
 
@@ -143,41 +156,62 @@ function drawBoidBugs() {
     });
 }
 
-function cohesion(bug) {
-    let cohesionVector = new Vector();   
-    boidBugs.forEach((boidBug) => {
-        if (boidBug != bug) {
-            cohesionVector.add(boidBug.position);
-        }
-    })
-    cohesionVector.divideBy(boidBugs.length - 1);
-    cohesionVector.subtract(bug.position);
-    cohesionVector.divideBy(COHESION_FACTOR);
-    return cohesionVector;
-}
-
-function separation(bug) {
+function avoid(bug) {
     let separationVector = new Vector();
     boidBugs.forEach((boidBug) => {
         if (boidBug != bug) {
-            if ( Vector.subtract(bug.position, boidBug.position).getMagnitude() < 20) {
+            if ( Vector.distance(bug.position, boidBug.position) < AVOID_NEIGHBOR_DISTANCE ) {
                 separationVector.subtract( Vector.subtract(boidBug.position, bug.position) );
             }
         }
     })
-    separationVector.divideBy(SEPARATION_FACTOR);
+    separationVector.multiplyBy(AVOIDANCE_FACTOR);
     return separationVector;
 }
 
-function alignment(bug) {
-    let alignmentVector = new Vector();    
+
+function align(bug) {
+    let alignmentVector = new Vector();
+    let neighborCount = 0;
     boidBugs.forEach((boidBug) => {
         if (boidBug != bug) {
-            alignmentVector.subtract(boidBug.vector);
+            if ( Vector.distance(bug.position, boidBug.position) < ALIGN_NEIGHBOR_DISTANCE ) {
+                alignmentVector.subtract(boidBug.vector);
+                neighborCount++;
+            }
         }
-    })    
-    alignmentVector.divideBy(boidBugs.length - 1);
-    alignmentVector.subtract(bug.vector);
-    alignmentVector.divideBy(ALIGNMENT_FACTOR);
+    })
+    if (neighborCount > 0) alignmentVector.divideBy(neighborCount);
+    //alignmentVector.subtract(bug.vector);
+    alignmentVector.multiplyBy(ALIGNMENT_FACTOR);
     return alignmentVector;
+}
+
+function approach(bug) {
+    let cohesionVector = new Vector();   
+    let neighborCount = 0;    
+    boidBugs.forEach((boidBug) => {
+        if (boidBug != bug) {
+            if ( Vector.distance(bug.position, boidBug.position) < APPROACH_NEIGHBOR_DISTANCE) {
+                cohesionVector.add(boidBug.position);
+                neighborCount++;
+            }
+        }
+    })
+    if (neighborCount > 0) {        
+        cohesionVector.divideBy(neighborCount);
+        cohesionVector.subtract(bug.position);
+        cohesionVector.multiplyBy(COHESION_FACTOR);
+    }
+    return cohesionVector;
+}
+
+function avoidWalls(bug) {
+    let avoidWallsVector = new Vector();
+
+    avoidWallsVector.add(new Vector(1 / (bug.position.x - fieldWidth + 1), 1 / (bug.position.y + 1)));
+    avoidWallsVector.add(new Vector(1 / (bug.position.x + 1), 1 / (bug.position.y - fieldHeight + 1)));    
+    avoidWallsVector.multiplyBy(AVOID_WALL_FACTOR);
+
+    return avoidWallsVector;
 }
